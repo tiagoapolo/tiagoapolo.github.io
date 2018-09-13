d7749b15a9c63b92fc7f44dc0ea000f97abe05ed
Tiago Paiva <tiagoapolo@gmail.com>
2018-09-13 17:30:23 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Intro from './intro/Intro';
+
+class App extends Component {
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Intro></Intro>
+        </header>
+        <div style={{ height: 680, width: '100%' }}></div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/intro/Intro.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Intro.css';
+
+class Intro extends Component {
+
+
+  render() {
+    return (
+      <section className="Intro">
+        <div className="top bg"></div>
+        <div className="middle">
+            <div className="profile">
+                <div className="bar">
+                    <ul>
+                        <li><a href="www.google.com">Trabalhos</a></li>
+                        <li><a href="www.google.com">Artigos</a></li>
+                        <li><a href="www.google.com">Contato</a></li>
+                        {/* <li><a href="www.google.com"><FontAwesomeIcon icon="github"/></a></li> */}
+                        <li>
+                            <a href="https://github.com/tiagoapolo" target="_blank" rel="noopener noreferrer">
+                                <i style={{ fontSize: 'large', paddingLeft: '1em' }} className="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/tiagoapolo" target='_blank' rel="noopener noreferrer">
+                                <i style={{ fontSize: 'large', color: '#1DA1F2' }} className="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://linkedin.com/in/tiago-fonseca/" target='_blank' rel="noopener noreferrer">
+                                <i style={{ fontSize: 'large', color: '#0077B5' }} className="fab fa-linkedin"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="avatar av-left"></div>
+            </div>
+        </div>
+        {/* <div className="bottom"> */}
+            {/* <h4>asijdoaijdoadjidjodias oaidjaoijdaoija aoijdaosijd</h4> */}
+        {/* </div> */}
+      </section>
+    );
+  }
+}
+
+export default Intro;
