5d5f113f3b100b7c2263694378f2fc83b6495668
Tiago Paiva <tiagoapolo@gmail.com>
2018-11-14 13:52:22 -0200

    solve the intro height for mobile

--- src/Jobs/Jobs.js
+++ src/Jobs/Jobs.js
@@ -30,17 +30,30 @@ class Jobs extends Component {
     render() {
     return (
       <div className="full-header">
-        <div className="container">           
+        <div>
+            <div style={{"margin-bottom":"1em"}}>
+                <img alt="" src="https://d2sef4u61egoux.cloudfront.net/assets/shared/partner-profile/experience-26084a9aadcc733eee381f5850d8ba8b096a640303d7f99e6a8b9b5b5f04ede2.svg"/>
+                <div>EXPERIENCE</div>
+            </div>
+        </div>
+        <div className="container timeline">       
             <section className="timeline_entry">
-                <h1>TESTE 1</h1>
+                <h1 className="jobtitle">TESTE 1</h1>
                 <div className="multiline">
-                    • Implemented DevOps strategies creating Kubernetes clusters and automated pipelines to reduce deployment time and friction. • Developed a support ticket activity monitoring mobile app to help managers reduce their time to action by 30% to assure the quality of service in the B2B internet provider sector. • Designed and implemented a project management dashboard to enable a distributed lab personnel work on remote projects and managers to track the project's daily activities Tech Stack: NodeJS, Angular 6, ReactJS, MongoDB, ParseDB, RabbitMQ, Docker, OpenStack, Docker, Swagger, Kubernetes.
+                    <p>• Implemented DevOps strategies creating Kubernetes clusters and automated pipelines to reduce deployment time and friction.</p>
+                    <p>• Developed a support ticket activity monitoring mobile app to help managers reduce their time to action by 30% to assure the quality of service in the B2B internet provider sector.</p>
+                    <p>• Designed and implemented a project management dashboard to enable a distributed lab personnel work on remote projects and managers to track the project's daily activities</p>
+                    <p>Tech Stack: NodeJS, Angular 6, ReactJS, MongoDB, ParseDB, RabbitMQ, Docker, OpenStack, Docker, Swagger, Kubernetes.</p>
                 </div>
             </section>
-            <section className="timeline_entry_2">
-                <h1>TESTE 1</h1>
+            <section className="timeline_entry">
+                <h1 className="jobtitle">TESTE 1 <span>– Software Developer</span></h1>
+                <div className="jobdate">APR – NOV 2017</div>
                 <div className="multiline">
-                    • Implemented DevOps strategies creating Kubernetes clusters and automated pipelines to reduce deployment time and friction. • Developed a support ticket activity monitoring mobile app to help managers reduce their time to action by 30% to assure the quality of service in the B2B internet provider sector. • Designed and implemented a project management dashboard to enable a distributed lab personnel work on remote projects and managers to track the project's daily activities Tech Stack: NodeJS, Angular 6, ReactJS, MongoDB, ParseDB, RabbitMQ, Docker, OpenStack, Docker, Swagger, Kubernetes.
+                    <p>• Implemented DevOps strategies creating Kubernetes clusters and automated pipelines to reduce deployment time and friction.</p>
+                    <p>• Developed a support ticket activity monitoring mobile app to help managers reduce their time to action by 30% to assure the quality of service in the B2B internet provider sector.</p>
+                    <p>• Designed and implemented a project management dashboard to enable a distributed lab personnel work on remote projects and managers to track the project's daily activities</p>
+                    <p>Tech Stack: NodeJS, Angular 6, ReactJS, MongoDB, ParseDB, RabbitMQ, Docker, OpenStack, Docker, Swagger, Kubernetes.</p>
                 </div>
             </section>
         </div>
